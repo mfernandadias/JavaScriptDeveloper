b369b36272cd634818d82b5de33dd34334566939
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-18 23:25:28 -0300

    if e else 2

---
+++ trilhaJavaScript/modulo1/2estruturacondicionais/if&else.js
@@ -0,0 +1,14 @@
+const numero = 15; 
+const numeroPar = (numero % 2 ) == 0;
+
+if (numeroPar) {
+    console.log('Executei');
+} else {
+    console.log('Impar');   
+} 
+
+/* ! ndica negacação
+if (!numeroPar) {
+    console.log('Impar');
+} */
+
