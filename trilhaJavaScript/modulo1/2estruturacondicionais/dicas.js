cd0aee97203be8c31e0d8aca660c6f8444a743ed
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-18 23:29:23 -0300

    dicas

---
+++ trilhaJavaScript/modulo1/2estruturacondicionais/dicas.js
@@ -0,0 +1,14 @@
+/* 
+== 
+===
+
+*/
+
+const numero = 5; 
+const oNumeroEPar = (numero % 2) === 0;
+
+if ((numero % 2) === 0) {
+    console.log('O número é par');
+} else {
+    console.log('O número é impar');
+}
