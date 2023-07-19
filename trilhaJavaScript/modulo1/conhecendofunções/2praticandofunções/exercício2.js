5d79c0bef7cdd8cd66ac03e33155f72c395f120a
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-19 16:23:21 -0300

    exercício 2
    verificar idade

---
+++ trilhaJavaScript/modulo1/conhecendofunções/2praticandofunções/exercício2.js
@@ -0,0 +1,20 @@
+/* 
+
+*/
+
+function escrevaMeuNome(nome) {
+    console.log('Meu nome é ' + nome);
+}
+
+escrevaMeuNome('Vitor');
+escrevaMeuNome('Renan');
+
+function verificarIdade(idade) {
+    if (idade >= 18) {
+        console.log('Maior');
+    } else {
+        console.log('Menor');
+    }
+}
+
+verificarIdade(11);
