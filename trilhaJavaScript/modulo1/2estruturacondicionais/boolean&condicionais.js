d63b6d90cfb5861454a683d0ca9bcde273a36fa7
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-18 22:40:22 -0300

    condicionais

---
+++ trilhaJavaScript/modulo1/2estruturacondicionais/boolean&condicionais.js
@@ -0,0 +1,13 @@
+
+const camisetaeAzul = true; 
+const camisetaLemaoAzul = false;
+
+const numero = 11; 
+// === condicional true/false
+const numeroPar = numero % 2 === 0; 
+
+
+console.log(numeroPar);
+
+/* há diverença de == e ===  
+*/
