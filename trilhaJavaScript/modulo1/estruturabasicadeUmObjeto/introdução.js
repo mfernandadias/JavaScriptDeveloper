75fc78e8475a8ad237714af8ccce91548cf919d0
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-19 20:00:26 -0300

    acessando dinamicamente valores de um objeto

--- trilhaJavaScript/modulo1/estruturabasicadeUmObjeto/introdução.js
+++ trilhaJavaScript/modulo1/estruturabasicadeUmObjeto/introdução.js
@@ -7,11 +7,19 @@ const pessoa = {
         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
 };
+
+const atributo = 'idade';
+//através de uma string estou acessando dinamicamente o atributo 
+console.log(pessoa['nome']);
+
+pessoa.nome = 'teste';
+
+/*
 pessoa.descrever = function() {
     console.log(`Meu nome é ${this.nome}`);
 }
 
-pessoa.descrever();
+pessoa.descrever(); */
 
 
 
