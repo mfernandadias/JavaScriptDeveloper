5a816a6d3acaa1d5d7ea510164d85cb3d5601e9c
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-20 23:39:52 -0300

    carro parte 2

--- trilhaJavaScript/modulo1/estruturabasicadeUmObjeto/3praticandocomObjetoseClasses/1carro.js
+++ trilhaJavaScript/modulo1/estruturabasicadeUmObjeto/3praticandocomObjetoseClasses/1carro.js
@@ -14,7 +14,13 @@ class Carro {
         this.cor = cor;
         this.gastoMedioPorKm = gastoMedioPorKm;
     }
+    calcularGastoDePercurso(distanciaEmKm, precoCombustivel) {
+        return distanciaEmKm * this.gastoMedioPorKm * precoCombustivel;
+    }
 }
 
 const uno = new Carro('Fiat','Prata', 1/12);
-console.log(uno); 
+uno.calcularGastoDePercurso(70, 5);
+console.log(uno.calcularGastoDePercurso(70, 5));
+const palio = new Carro('Fiat', 'Preto', 1/10);
+console.log(palio.calcularGastoDePercurso(70/5));
