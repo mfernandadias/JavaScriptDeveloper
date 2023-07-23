d3a8f61ec849766a3bf248b9593197e0432ad753
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-22 22:39:32 -0300

    exercício com maior número sorteado

---
+++ trilhaJavaScript/modulo1/importaçãoeexportaçãocomJavascript/02exermaiornumerosorteado.js
@@ -0,0 +1,13 @@
+/* uma sala contem 5 alunos e para aluno foi sorteado um número de 1 - 100
+faça um programa que receba os 5 números aorteados para alunos e mostre o maior número sorteado 
+
+dados de entrada 
+5
+50 
+10 
+98 
+23
+
+saída 
+98 
+*/
--- trilhaJavaScript/modulo1/importaçãoeexportaçãocomJavascript/main.js
+++ trilhaJavaScript/modulo1/importaçãoeexportaçãocomJavascript/main.js
@@ -1,13 +1,9 @@
 
 const {gets, print } = require('./funçõesauxiliares');
 
-
 print(gets());
-
-
-/*const pessoa = {
-    nome: 'vitor'
-};
-
-const { nome } = pessoa; 
-const nome = pessoa.nome; */
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+print(gets());
