09b5c87dfd95e9990a32e613495e20adfc024338
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-22 22:47:51 -0300

    maior valor concluido

--- trilhaJavaScript/modulo1/importaçãoeexportaçãocomJavascript/main.js
+++ trilhaJavaScript/modulo1/importaçãoeexportaçãocomJavascript/main.js
@@ -1,9 +1,20 @@
 
 const {gets, print } = require('./funçõesauxiliares');
 
-print(gets());
-print(gets());
-print(gets());
-print(gets());
-print(gets());
-print(gets());
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    numerosSorteados.push(numeroSorteado);
+}
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteado = numerosSorteados[i];
+    if (numeroSorteado > maiorValor) {
+        maiorValor = numeroSorteado;
+    }
+}
+
+print(maiorValor);
