646373779571fadc078cda0b7812729397090003
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-27 01:30:07 -0300

    anotações

--- trilhaJavaScript/modulo3/02orientaçãoaprotocolo/operadornew.js
+++ trilhaJavaScript/modulo3/02orientaçãoaprotocolo/operadornew.js
@@ -15,8 +15,22 @@ console.log(new Pessoa('renan', 30))
 renan.falar() */
 
 //protótipo da string 
-String.prototype.toPlang = function() {
+/*String.prototype.toPlang = function() {
     return `p ${this}`
 }
 
-console.log('teste'.toPlang())
+console.log('teste'.toPlang()) */
+
+//O que são protótipos?
+//protótipos são o mecanismo pelo qual objetos JavScript herdam recuros uns dos outros 
+
+function Pessoa(nome) {
+    this.nome = nome
+}
+
+Pessoa.prototype.falarSeuNome = function () {
+    console.log(`Meu nome é ${this.nome}`)
+}
+
+const renan = new Pessoa('renan')
+renan.falarSeuNome()
