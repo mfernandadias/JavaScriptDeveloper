c2c86ea16b15f4145d99b807351bc467aaca1956
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-27 22:50:59 -0300

    function
    first class functions, highert order functions, function declaration e function expression

---
+++ trilhaJavaScript/modulo3/03funções/firstClass.js
@@ -0,0 +1,23 @@
+//first Class functions, higher order functions, function declaration e function expresion 
+
+/*function falarMeuNome( ) {
+    console.log('Meu nome é Renan')
+}
+
+function falarMeuNomeCompleto(falarMeuNome) {
+    falarMeuNome()
+    console.log('Johannses de Paula')
+}
+
+falarMeuNomeCompleto(falarMeuNome) */
+
+function nomeDaFuncao() {
+    console.log('nomeDaFuncao')
+}
+
+const nomeDeOutraFuncao = function () {
+    console.log('nomeDeOutraFuncao')
+}
+
+nomeDaFuncao()
+nomeDeOutraFuncao()
