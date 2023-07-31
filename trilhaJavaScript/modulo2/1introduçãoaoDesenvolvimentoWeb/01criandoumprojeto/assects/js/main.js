3fb6cb7cafeeb23e5ae14ff81f52b6b0f65ec06e
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-31 20:05:56 -0300

    promise
    manipulando o resultado da requisição através de uma Promise

--- trilhaJavaScript/modulo2/1introduçãoaoDesenvolvimentoWeb/01criandoumprojeto/assects/js/main.js
+++ trilhaJavaScript/modulo2/1introduçãoaoDesenvolvimentoWeb/01criandoumprojeto/assects/js/main.js
@@ -4,6 +4,34 @@ const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 fetch(url)
+     .then((response) => response.json())
+     .then((jsonBody) => console.log(jsonBody))
+     .catch((erro) => console.error(error))
 
-const x = 10 + 10
-console.log(x)
+
+
+
+
+//interface de alguma coisa 
+/*fetch(url)
+.then(function (response){
+   return response.json()
+})
+.then(function (jsonBody) {
+    console.log(jsonBody)
+})
+.catch(function(error) {
+    console.log(error)
+})
+.fenally(function () {
+    console.log('Requisição concluída')
+}) */
+
+
+/*try {
+
+} catch (erroo) {
+    
+} finally {
+
+} */
