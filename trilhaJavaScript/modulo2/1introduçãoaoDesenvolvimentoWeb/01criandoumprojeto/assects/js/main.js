6c6f62357426ddb8ad8151573d0294f7f4e9c7ba
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-07-31 21:15:57 -0300

    lista de HTML

--- trilhaJavaScript/modulo2/1introduçãoaoDesenvolvimentoWeb/01criandoumprojeto/assects/js/main.js
+++ trilhaJavaScript/modulo2/1introduçãoaoDesenvolvimentoWeb/01criandoumprojeto/assects/js/main.js
@@ -3,11 +3,25 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+
+
+
+
+
+
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
      .then((response) => response.json())
-     .then((jsonBody) => console.log(jsonBody))
-     .catch((erro) => console.error(error))
+     .then((jsonBody) => jsonBody.results)
+     .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemonList[i];
+            pokemonList.innerHtml += convertPokemonToLi(pokemon)
+        }
+     })
 
+     .catch((error) => console.error(error))
 
 
 
